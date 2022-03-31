85cfccecd1fc26a16fc316e99ab1b04aa38b6359
Juan Barroso <jbarroso@mymoid.com>
2022-03-31 15:41:25 +0100

    Reduce text on types details

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,28 +1,46 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React from "react"
-import { FaHeart , FaRegHeart } from 'react-icons/fa';
-import './Pokemon.css'
+import React from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import "./Pokemon.css";
 
-const Pokemon = ({image , name , types , isFavourite}) => {
- 
-    return(<div className="PokemonRow">
-       <img  className="PokemonImage" src ={image}/>
-       <div className="cardFotter">
-           <div className="textName">{name}</div>
-           <div className="textType">{types.map((type , index)=> {
-            debugger 
-             return (
-                    
-                     types.length === index ?
-                     `${type}, ` :
-                      type 
-                )})}    
-            </div>
-            {isFavourite ? 
-           <div className="favouriteIcon" ><FaHeart/></div> : 
-           <div className="favouriteIcon"><FaRegHeart/></div>}    
-       </div> 
-    </div>)
+function typesTextTransformation(types) {
+  const initialValue = "";
+
+  const textTypes = types.reduce(
+    (previousValue, currentValue, currentIndexOptional) => {
+      currentValue = !isLatest(currentIndexOptional, types.length)
+        ? `${currentValue}, `
+        : currentValue;
+      return `${previousValue}${currentValue}`;
+    },
+    initialValue
+  );
+  return textTypes;
 }
 
-export default Pokemon
+function isLatest(current, collection) {
+  return current + 1 === collection;
+}
+
+const Pokemon = ({ image, name, types, isFavourite }) => {
+  return (
+    <div className="PokemonRow">
+      <img className="PokemonImage" src={image} />
+      <div className="cardFotter">
+        <div className="textName">{name}</div>
+        <div className="textType">{typesTextTransformation(types)}</div>
+        {isFavourite ? (
+          <div className="favouriteIcon">
+            <FaHeart />
+          </div>
+        ) : (
+          <div className="favouriteIcon">
+            <FaRegHeart />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
