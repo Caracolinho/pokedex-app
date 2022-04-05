6ab7bd1c0c89ddb9e393d1c5d50e624fcb30ae29
Caracolinho26 <tiagocaracol62@gmail.com>
2022-04-05 08:51:57 +0100

    Api connect

---
+++ src/Api/PokemonApi.js
@@ -0,0 +1,35 @@
+function getPokemos() {
+  const pokemonQuery = `
+        query {
+            pokemons(query: { limit: 10, offset: 0, search: "" }) {
+                limit
+                offset
+                count
+                edges {
+                    id
+                    name
+                    types
+                    isFavorite
+                    image
+                }
+            }
+        }
+      `;
+
+  return window
+     .fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        query: pokemonQuery,
+      }),
+    })
+    .then((r) => r.json())
+    .then((response) => {
+      return response.data.pokemons.edges;
+    });
+}
+
+export { getPokemos };
--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import React, { Component } from 'react';
 import Pokemon from './components/Pokemon';
- 
+import {getPokemos} from './Api/PokemonApi'
 import './App.css';
 
-const pokemons = [
-  {
-    id: "001",
-    image: 'https://img.pokemondb.net/artwork/bulbasaur.jpg',
-    name: "Bulbasaur",
-    classification: "Seed Pokemon",
-    type: ["Grass","Poison"],
-    isFavorite: true
-  },
-  {
-    id: "001",
-    image: 'https://img.pokemondb.net/artwork/bulbasaur.jpg',
-    name: "Bulbasaur",
-    classification: "Seed Pokemon",
-    type: ["Grass","Veneno","Mentol"],
-    isFavorite: true
-  },
-  {
-    id: "001",
-    image: 'https://img.pokemondb.net/artwork/bulbasaur.jpg',
-    name: "Bulbasaur",
-    classification: "Seed Pokemon",
-    type: ["Grass"],
-    isFavorite: true
-  },
-  {
-    id: "001",
-    image: 'https://img.pokemondb.net/artwork/bulbasaur.jpg',
-    name: "Bulbasaur",
-    classification: "Seed Pokemon",
-    type: ["Grass","Veneno","Fogo","Silva"],
-    isFavorite: true
-  }
- 
-]
+const pokemons = getPokemos()
+
+debugger
 
 class App extends Component {
   render() {
