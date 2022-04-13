2618338f6479727365c0e437639ae3aa06b8941e
Juan Barroso <jbarroso@mymoid.com>
2022-04-13 12:50:41 +0100

    Return page not found

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
 import Pokemons from './screens/Pokemons'
 import Pokemon from './screens/Pokemon'
+import NotFound from './screens/NotFound'
 import './App.css'
 
 class App extends Component {
 
   pathname = window.location.pathname
 
-  render() {
 
+  getPage = () => {
+    if (this.pathname === "/pokemons") {
+      return <Pokemons/>
+    }
+    if(this.pathname === "/pokemon") {
+      return <Pokemon/>
+    }
+    return <Pokemon/>
+  }
+
+  render() {
+    
+    
     return (
       <div className="App">
         <div>
-          {this.pathname === "/pokemons" ? <Pokemons/> : <Pokemon/>}
+          {this.getPage()}
         </div>
       </div>
     );
