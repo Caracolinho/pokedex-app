f7a1a8a0f3af4675fec2823298fde4f488f50893
Juan Barroso <jbarroso@mymoid.com>
2022-04-13 13:15:05 +0100

    Introduce router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React, { Component } from 'react'
-import Pokemons from './screens/Pokemons'
-import Pokemon from './screens/Pokemon'
-import NotFound from './screens/NotFound'
-import './App.css'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Pokemons from "./screens/Pokemons";
+import Pokemon from "./screens/Pokemon";
+import NotFound from "./screens/NotFound";
+import "./App.css";
 
-class App extends Component {
-
-  pathname = window.location.pathname
-
-
-  getPage = () => {
-    if (this.pathname === "/pokemons") {
-      return <Pokemons/>
-    }
-    if(this.pathname === "/pokemon") {
-      return <Pokemon/>
-    }
-    return <Pokemon/>
-  }
-
-  render() {
-    
-    
-    return (
-      <div className="App">
-        <div>
-          {this.getPage()}
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/pokemons" element={<Pokemons />} />
+          <Route path="/pokemon" element={<Pokemon />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
-
