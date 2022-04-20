d6aad91922206962b36eead9b0b7ed1e3b46774a
Juan Barroso <jbarroso@mymoid.com>
2022-04-20 16:06:39 +0100

    Remove comment example

--- src/screens/Pokemons.jsx
+++ src/screens/Pokemons.jsx
@@ -48,24 +48,4 @@ function Pokemons() {
   );
 }
 
-// Example
-
-{
-  /* <InfiniteScroll
-dataLength={this.state.items.length}
-next={this.fetchMoreData}
-style={{ display: 'flex', flexDirection: 'column-reverse' }} //To put endMessage and loader to the top.
-inverse={true} //
-hasMore={true}
-loader={<h4>Loading...</h4>}
-scrollableTarget="scrollableDiv"
->
-{this.state.items.map((_, index) => (
-  <div style={style} key={index}>
-    div - #{index}
-  </div>
-))}
-</InfiniteScroll> */
-}
-
 export default Pokemons;
